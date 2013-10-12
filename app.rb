4296569c667f910a150bd1462e5ff163867e062d
Miles Gould <miles@assyrian.org.uk>
2013-10-13 00:17:36 +0100

    Form for width/length/diameter.

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 set :haml, :format => :html5
 
-# how many circles can you fit into a width * height rectangle, if plants
+# how many circles can you fit into a width * length rectangle, if plants
 # must be 2y apart and you are using a hexagonal lattice?
 # For a derivation, see
 # https://twitter.com/pozorvlak/status/388056883153473538/photo/1
-def fit_hexagons(width, height, y)
+def fit_hexagons(width, length, y)
   x = y/Math.sqrt(3)                # half the width of a hexagon side
-  num_rows = (height / y - 1).floor
+  num_rows = (length / y - 1).floor
   num_cols = ((width / x - 1)/3).floor
   if num_rows.even?
-    # all columns have equal height
+    # all columns have equal length
     return num_rows * num_cols
   else
     # odd-numbered columns are one hex taller
@@ -19,16 +19,18 @@ def fit_hexagons(width, height, y)
 end
 
 get '/' do
-  width = params[:width].to_f
-  height = params[:height].to_f
-  y = params[:y].to_f
-  unless (y.nil? || width.nil? || height.nil?)
-    num_hexes = fit_hexagons(width, height, y)
+  width = params[:width]
+  length = params[:length]
+  diameter = params[:diameter]
+  unless width.nil? or length.nil? or diameter.nil?
+    num_hexes = fit_hexagons(width.to_f, length.to_f, diameter.to_f / 2)
+  else
+    num_hexes = nil
   end
   haml :index, :locals => {
     :width => width,
-    :height => height,
-    :y => y,
+    :length => length,
+    :diameter => diameter,
     :num_hexes => num_hexes
   }
 end
